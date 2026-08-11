64e3ccafc5a2e0004059f8738cf1184465b58ad9
comercialexternobv-png <comercialexterno.bv@gmail.com>
2026-08-10 21:17:36 -0300

    feat: add official partner course catalog links

---
+++ src/data/partnerCatalogs.ts
@@ -0,0 +1,11 @@
+export const partnerCatalogs = {
+  uninassauGraduacao: 'https://graduacao.uninassau.digital/nossos-cursos',
+  uninassauPos: 'https://ead.uninassau.edu.br/pos-digital',
+  unifaelPos: 'https://posgrad.unifael.edu.br/digital?polo=boaviagemcentroii-ce&tipo=digital',
+} as const;
+
+export const partnerCatalogLabels = {
+  uninassauGraduacao: 'Graduação UNINASSAU',
+  uninassauPos: 'Pós-graduação UNINASSAU',
+  unifaelPos: 'Pós-graduação UNIFAEL',
+} as const;
